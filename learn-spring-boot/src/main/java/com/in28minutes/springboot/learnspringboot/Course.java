2e43d8d2adf611e15d6a2f51b99c409e24f9c01f
ldh9282 <ldh9282@naver.com>
2023-02-09 18:43:53 +0900

    section5

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
@@ -1,12 +1,12 @@
 package com.in28minutes.springboot.learnspringboot;
 
 public class Course {
-
 	private long id;
 	private String name;
 	private String author;
-	
+
 	public Course(long id, String name, String author) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.author = author;
@@ -28,7 +28,5 @@ public class Course {
 	public String toString() {
 		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
 	}
-	
-	
-	
+
 }
