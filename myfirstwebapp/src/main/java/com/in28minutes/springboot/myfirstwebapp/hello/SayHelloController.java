4b7997a0fe317fc7e3db9aa30870eb7757248fde
ldh9282 <ldh9282@naver.com>
2023-02-15 00:01:48 +0900

    section7

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -6,39 +6,41 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SayHelloController {
-
-	@RequestMapping("say-hello") 
+	
+	//"say-hello" => "Hello! What are you learning today?"
+	
+	//say-hello
+	// http://localhost:8080/say-hello
+	@RequestMapping("say-hello")
 	@ResponseBody
 	public String sayHello() {
 		return "Hello! What are you learning today?";
 	}
 	
-	@RequestMapping("say-hello-html") 
+	@RequestMapping("say-hello-html")
 	@ResponseBody
 	public String sayHelloHtml() {
 		StringBuffer sb = new StringBuffer();
-		sb.append("<html>\r\n"
-				+ "\r\n"
-				+ "    <head>\r\n"
-				+ "    <title>My first HTML Page</title>\r\n"
-				+ "    </head>\r\n"
-				+ "\r\n"
-				+ "    <body>\r\n"
-				+ "        <h1>\r\n"
-				+ "            My first html page with body\r\n"
-				+ "        </h1>\r\n"
-				+ "    \r\n"
-				+ "    </body>\r\n"
-				+ "\r\n"
-				+ "</html>");
+		sb.append("<html>");
+		sb.append("<head>");
+		sb.append("<title> My First HTML Page - Changed</title>");
+		sb.append("</head>");
+		sb.append("<body>");
+		sb.append("My first html page with body - Changed");
+		sb.append("</body>");
+		sb.append("</html>");
+		
 		return sb.toString();
 	}
 	
-	@RequestMapping("say-hello-jsp") 
+	//
+	// "say-hello-jsp" => sayHello.jsp 
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+	// /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+	@RequestMapping("say-hello-jsp")
 	public String sayHelloJsp() {
 		return "sayHello";
 	}
-	
 }
-
-
